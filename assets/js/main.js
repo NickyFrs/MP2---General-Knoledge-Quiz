eba66ef0c6df321eb8223573023cbbcb7e2220f5
Nicky Frias <nickyfrs@gmail.com>
2022-04-12 13:22:57 +0100

    quiz page - added a progress bar and did some styling to it. started the JS code.

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,13 @@
 
+// // JQuery code
+// $(document).ready(function(){/*Code goes here*/});
 
-// JQuery code
-$(document).ready(function(){/*Code goes here*/});
+const questionsContainer = document.getElementById("questions-container");
+const answerEls = document.querySelectorAll(".answer");
+const questionEl = document.getElementById("question");
+const a_text = document.getElementById("a_text");
+const b_text = document.getElementById("b_text");
+const c_text = document.getElementById("c_text");
+const d_text = document.getElementById("d_text");
+const submitBtn = document.getElementById("submit");
+const progressBarFull = document.getElementById("progress-bar-full");
