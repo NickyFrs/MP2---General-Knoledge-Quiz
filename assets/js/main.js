332f19da65c5daac70f4f181f4058637551e3723
Nicky Frias <nickyfrs@gmail.com>
2022-04-13 06:08:31 +0100

    quiz page - worked on JS file. added the questions and answers object with a key:value pair structure.

--- assets/js/main.js
+++ assets/js/main.js
@@ -2,12 +2,96 @@
 // // JQuery code
 // $(document).ready(function(){/*Code goes here*/});
 
+// Array of question for the quiz. Te quiz data = Q&A
+
+const questionsData = [
+    {
+        question: "Pregunta A",
+        a: "respuesta a",
+        b: "respuesta b",
+        c:" respuesta c",
+        d: "respuesta d",
+        correct: "respuesta correcta"
+    },
+
+    {
+        question: "Pregunta B",
+        a: "respuesta a",
+        b: "respuesta b",
+        c: "respuesta c",
+        d: "respuesta d",
+        correct: "respuesta correcta"
+    },
+
+    {
+        question: "Pregunta C",
+        a: "respuesta a",
+        b: "respuesta b",
+        c: "respuesta c",
+        d: "respuesta d",
+        correct: "respuesta correcta"
+    },
+
+    {
+        question: "Pregunta D",
+        a: "respuesta a",
+        b: "respuesta b",
+        c: "respuesta c",
+        d: "respuesta d",
+        correct: "respuesta correcta"
+    }
+]
+
+// All constant variables to manipulate the DOM
+
 const questionsContainer = document.getElementById("questions-container");
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
+const answers = document.querySelectorAll(".answer");
+const question = document.getElementById("question");
+const optionA = document.getElementById("option-a");
+const optionB = document.getElementById("option-b");
+const optionC = document.getElementById("option-c");
+const optionD = document.getElementById("option-d");
 const submitBtn = document.getElementById("submit");
+
 const progressBarFull = document.getElementById("progress-bar-full");
+
+let activeQuiz = 0;
+let score = 0;
+
+
+function startQuiz() {
+    clearAnswerSelected();
+
+    const questionArray = questionsData[activeQuiz];
+
+    question.innerText = questionArray.question;
+    optionA.innerText = questionArray.a;
+    optionB.innerText = questionArray.b;
+    optionC.innerText = questionArray.c;
+    optionD.innerText = questionArray.d;
+}
+
+// Function to clear the selected answer once the next quiz's question is presented
+function clearAnswerSelected() {
+    answers.forEach(answer => answer.checked = false)
+};
+
+// Function to
+function selctedAnswer () {
+    let selectedOption;
+
+    answers.forEach(answer => {
+        if (answer.checked) {
+            selectedOption = answer.id
+        }
+    })
+
+    return selectedOption;
+}
+
+
+// Event listening event for the click on the submit button to check the option selected
+submitBtn.addEventListener('click', () => {
+    const answer = selctedAnswer();
+
+})
